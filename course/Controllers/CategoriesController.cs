711517f5f257f7c8ab44a5f9bff7658744777ccd
Luis Alfredo <luisalfredopersonal@gmail.com>
2023-08-16 23:56:39 -0300

    Add project files.

---
+++ course/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using course.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace course.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private List<Category> categories = new List<Category>()
+        {
+            new Category()
+            {
+                Id = 0,
+                Name = "Apartment",
+                ImageUrl = "appartment.png"
+            }
+        };
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return categories;
+        }
+
+
+    }
+}
