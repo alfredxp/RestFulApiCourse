08851de8e9bd79e0a54770d6b646b771795def16
luisalfredox <luisalfredopersonal@gmail.com>
2023-08-29 14:11:42 -0400

    Feat: Categories Controller authorization

---
+++ course/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using course.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace course.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        ApiDbContext _db = new ApiDbContext();
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok(_db.Categories);
+        }
+
+
+    }
+}
