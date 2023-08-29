260ac1aa15d1a31124c9fcb82f3719e6a31d1c87
luisalfredox <luisalfredopersonal@gmail.com>
2023-08-29 12:09:51 -0400

    Feat: Jwt Login

--- course/Controllers/UsersController.cs
+++ course/Controllers/UsersController.cs
@@ -2,6 +2,10 @@
 using course.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace course.Controllers
 {
@@ -10,6 +14,13 @@ namespace course.Controllers
     public class UsersController : ControllerBase
     {
         ApiDbContext _db = new ApiDbContext();
+        private IConfiguration _config;
+
+        public UsersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
 
         [HttpPost("[action]")]
         public IActionResult Register([FromBody] User user)
@@ -25,7 +36,29 @@ namespace course.Controllers
 
 
         }
+        [HttpPost("[action]")]
+        public IActionResult Login([FromBody] User user)
+        {
+            var currentUser = _db.Users.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+            if(currentUser == null)
+            {
+                return NotFound();
+            }
 
-
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, user.Email),  
+            };
+            var token = new JwtSecurityToken(
+                issuer: _config["JWT:Issuer"],
+                audience: _config["JWT:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(jwt);
+        }
     }
 }
